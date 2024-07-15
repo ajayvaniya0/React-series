eeeb569354491405b901102ebf8b1693f6b72e50
Ajay <ajayvaniya54@gmail.com>
2024-07-15 20:10:00 +0530

    Basic hello world type

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./Chai"
+
+function App() {
+  const username = "chai aur code"
+  
+
+  return (
+    <>
+      <Chai/>
+      <h2>Lets Learn React {username}</h2> // this is called Elavuated expression 
+      <p>Chai is ready</p>
+    </>
+  )
+}
+
+export default App
